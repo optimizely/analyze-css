6505aae291ad05425066788b40225a8b866a0d61
Daniel O'Connor <daniel@optimizely.com>
2015-04-16 21:01:59 -0700

    Clean up the parker executable call a little.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var exec = require('child_process').exec;
 
-exec('curl https://www.optimizely.com/signin -s | grep -o /master-[0-9]*\.[0-9]*/ | head -1 | awk \'{print "https://www.optimizely.com"$1"dist/css/app.css"}\' | xargs curl -s | node_modules/parker/parker.js -s --format=json', function (error, stdout, stderr) {
+exec('curl https://www.optimizely.com/signin -s | grep -o /master-[0-9]*\.[0-9]*/ | head -1 | awk \'{print "https://www.optimizely.com"$1"dist/css/app.css"}\' | xargs curl -s | ./node_modules/parker/parker.js -s --format=json', function (error, stdout, stderr) {
   // output is in stdout
   console.log(stdout);
 });
